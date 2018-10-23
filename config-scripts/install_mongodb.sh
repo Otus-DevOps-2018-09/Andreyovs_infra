b1ebf0a0724048e1f584a3ada7f46c8f3eae6c2a
andrey ovsyannikov <andreyovs@yandex.ru>
2018-10-23 23:59:41 +0300

    Fixed file broken permissions

--- config-scripts/install_mongodb.sh
+++ config-scripts/install_mongodb.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+<<<<<<< HEAD:config-scripts/install_mongodb.sh
 set -e
+=======
+
+>>>>>>> e145918... Fixed file broken permissions:install_mongodb.sh
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 sudo apt update
--- config-scripts/install_ruby.sh
+++ config-scripts/install_ruby.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+<<<<<<< HEAD:config-scripts/install_ruby.sh
 set -e
+=======
+>>>>>>> e145918... Fixed file broken permissions:install_ruby.sh
 sudo apt-get update
 sudo apt install -y ruby-full ruby-bundler build-essential
 gem install json -v '1.8.6'
@@ -8,6 +11,10 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update
+<<<<<<< HEAD:config-scripts/install_ruby.sh
 sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
+=======
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
+>>>>>>> e145918... Fixed file broken permissions:install_ruby.sh
 ruby -v
 bundle -v
