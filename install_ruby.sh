e145918802112314e52792a24d24605b06dc22ca
andrey ovsyannikov <andreyovs@yandex.ru>
2018-10-23 23:59:41 +0300

    Fixed file broken permissions

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-sudo apt-get install ruby-dev
+sudo apt-get update
+sudo apt install -y ruby-full ruby-bundler build-essential
 gem install json -v '1.8.6'
 sudo apt-get install build-essential
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
@@ -8,9 +8,5 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
-sudo apt update
-sudo apt install -y ruby-full ruby-bundler build-essential
 ruby -v
 bundle -v
-
-
