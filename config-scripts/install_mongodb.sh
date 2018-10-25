21a30388ca9c7b873e2c7956320ed8ea4c824b8f
andrey ovsyannikov <andreyovs@yandex.ru>
2018-10-25 15:11:52 +0300

    base homework compleeted

--- config-scripts/install_mongodb.sh
+++ config-scripts/install_mongodb.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 <<<<<<< HEAD:config-scripts/install_mongodb.sh
+<<<<<<< HEAD:config-scripts/install_mongodb.sh
 set -e
 =======
 
 >>>>>>> e145918... Fixed file broken permissions:install_mongodb.sh
+=======
+set -e
+>>>>>>> 47572de... base homework compleeted:config-scripts/install_mongodb.sh
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 sudo apt update
