8e6fe69d195c0bbcdee7a173c374d3144ffe70df
andrey ovsyannikov <andreyovs@yandex.ru>
2018-10-25 11:42:14 +0300

    Fixed rubby install.sh param -y

--- config-scripts/install_ruby.sh
+++ config-scripts/install_ruby.sh
@@ -12,9 +12,13 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update
 <<<<<<< HEAD:config-scripts/install_ruby.sh
+<<<<<<< HEAD:config-scripts/install_ruby.sh
 sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
 =======
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
 >>>>>>> e145918... Fixed file broken permissions:install_ruby.sh
+=======
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
+>>>>>>> 682cf4a... Fixed rubby install.sh param -y:install_ruby.sh
 ruby -v
 bundle -v
