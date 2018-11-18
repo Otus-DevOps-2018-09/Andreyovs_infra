d6e6a039b27b765282f341c6d1ec9df9f418b493
andreyovs <andreyovs@yandex.ru>
2018-11-18 11:03:48 +0300

    a-3 commit 10

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -38,3 +38,16 @@ resource "google_compute_firewall" "firewall_ssh" {
 
   source_ranges = "${var.source_ranges}"
 }
+
+resource "google_compute_firewall" "firewall_nginx" {
+  name    = "allow-nginx-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
